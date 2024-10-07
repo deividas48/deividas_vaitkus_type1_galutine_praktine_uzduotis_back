3166f37ea0004a77d04d65b600b329a2562d9b34
deividas48 <deividas.v6@gmail.com>
2024-10-07 20:52:48 +0300

    feat: PageAddListing added more images inputs to form

--- src/routes/listing/listingRoutesFormAndFiles.js
+++ src/routes/listing/listingRoutesFormAndFiles.js
@@ -37,97 +37,135 @@ const storage = multer.diskStorage({
   },
 });
 
-// Initializing Multer with the defined storage settings
-const upload = multer({ storage });
-
 // */ Manages files uploaded from the front-end
 
+// Initializing Multer ...
+const upload = multer({
+  // ... with the defined storage settings
+  storage,
+  // Define the fields (form) with their respective name attributes. It's from the front-end.
+}).fields([
+  { name: 'photoLMain', maxCount: 1 },
+  { name: 'photoL1', maxCount: 1 },
+  { name: 'photoL2', maxCount: 1 },
+  { name: 'photoL3', maxCount: 1 },
+  { name: 'photoL4', maxCount: 1 },
+  { name: 'photoL5', maxCount: 1 },
+  { name: 'photoL6', maxCount: 1 },
+  { name: 'photoL7', maxCount: 1 },
+  { name: 'photoL8', maxCount: 1 },
+  { name: 'photoL9', maxCount: 1 },
+]);
+
 // POST - creates a new listing with file (images) upload
-listingsRouterFormAndFiles.post(
-  '/',
-  upload.array('photos', 10),
-  async (req, res) => {
-    // req.files contains the array of uploaded files
-    // eslint-disable-next-line prefer-destructuring
+listingsRouterFormAndFiles.post('/', upload, async (req, res) => {
+  // req.files contains the array of uploaded files
+  // eslint-disable-next-line prefer-destructuring
 
-    // Files (images) from the front-end
-    const { files } = req; // or ≈ 'const files = req.files;'
+  // Files (images) from the front-end
+  const { files } = req; // or ≈ 'const files = req.files;'
 
-    const {
-      title,
-      description,
-      price,
-      phone,
-      type,
-      town_id,
-      user_id,
-      category_id,
-      is_published = 1,
-    } = req.body;
+  const {
+    title,
+    description,
+    price,
+    phone,
+    type,
+    town_id,
+    user_id,
+    category_id,
+    is_published = 1,
+  } = req.body;
 
-    // #1.2_Post. Ensure required fields are present
-    if (
-      !title
-      || !description
-      || !price
-      || !phone
-      || !type
-      || !town_id
-      || !category_id
-    ) {
-      return res.status(400).json({ error: 'Missing required fields' });
+  // #1.2_Post. Ensure required fields are present
+  if (
+    !title
+    || !description
+    || !price
+    || !phone
+    || !type
+    || !town_id
+    || !category_id
+  ) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  // Handle file uploads. It's only for files (images) naming.
+  // Each file will be in req.files under its field name
+  const main_image_url = req.files.photoLMain
+    ? req.files.photoLMain[0].filename
+    : null;
+  // Example: list_image_url_1 is set to '1638316800000-photo1.jpg'.
+  const list_image_url_1 = req.files.photoL1
+    ? req.files.photoL1[0].filename
+    : null;
+  const list_image_url_2 = req.files.photoL2
+    ? req.files.photoL2[0].filename
+    : null;
+  const list_image_url_3 = req.files.photoL3
+    ? req.files.photoL3[0].filename
+    : null;
+  const list_image_url_4 = req.files.photoL4
+    ? req.files.photoL4[0].filename
+    : null;
+  const list_image_url_5 = req.files.photoL5
+    ? req.files.photoL5[0].filename
+    : null;
+  const list_image_url_6 = req.files.photoL6
+    ? req.files.photoL6[0].filename
+    : null;
+  const list_image_url_7 = req.files.photoL7
+    ? req.files.photoL7[0].filename
+    : null;
+  const list_image_url_8 = req.files.photoL8
+    ? req.files.photoL8[0].filename
+    : null;
+  const list_image_url_9 = req.files.photoL9
+    ? req.files.photoL9[0].filename
+    : null;
+
+  const argArr = [
+    // id, // id is autoincremented, so we don't need to pass it
+    title,
+    main_image_url || null, // null - ensure main_image_url is not undefined,
+    // if it is, set it to null
+    description,
+    price,
+    phone,
+    type,
+    town_id || null, // null - ...
+    user_id || null, // null - ...
+    category_id,
+    is_published,
+    list_image_url_1 || null, // null - ...
+    list_image_url_2 || null, // null - ...
+    list_image_url_3 || null, // null - ...
+    list_image_url_4 || null, // null - ...
+    list_image_url_5 || null, // null - ...
+    list_image_url_6 || null, // null - ...
+    list_image_url_7 || null, // null - ...
+    list_image_url_8 || null, // null - ...
+    list_image_url_9 || null, // null - ...
+  ];
+
+  // Define the SQL query for inserting the listing into the database
+  const sql = `INSERT INTO skelbimai (${listingsColumns}) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+  // Unique variable names for row and error
+  try {
+    const [insertedRow, insertError] = await dbQueryWithData(sql, argArr);
+    if (insertError) {
+      console.warn('Error inserting listing:', insertError);
+      return res.status(400).json({ error: insertError.message });
     }
 
-    // Handle file uploads. It's only for files (images) naming.
-    const main_image_url = files[0] ? files[0].filename : null;
-    // Example: list_image_url_1 is set to '1638316800000-photo1.jpg'.
-    const list_image_url_1 = files[1] ? files[1].filename : null;
-    const list_image_url_2 = files[2] ? files[2].filename : null;
-    const list_image_url_3 = files[3] ? files[3].filename : null;
-
-    const argArr = [
-      // id, // id is autoincremented, so we don't need to pass it
-      title,
-      main_image_url || null, // null - ensure main_image_url is not undefined,
-      // if it is, set it to null
-      description,
-      price,
-      phone,
-      type,
-      town_id || null, // null - ...
-      user_id || null, // null - ...
-      category_id,
-      is_published,
-      list_image_url_1 || null, // null - ...
-      list_image_url_2 || null, // null - ...
-      list_image_url_3 || null, // null - ...
-      null, // list_image_url_4
-      null, // list_image_url_5
-      null, // list_image_url_6
-      null, // list_image_url_7
-      null, // list_image_url_8
-      null, // list_image_url_9
-    ];
-
-    // Define the SQL query for inserting the listing into the database
-    const sql = `INSERT INTO skelbimai (${listingsColumns}) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-    // Unique variable names for row and error
-    try {
-      const [insertedRow, insertError] = await dbQueryWithData(sql, argArr);
-      if (insertError) {
-        console.warn('Error inserting listing:', insertError);
-        return res.status(400).json({ error: insertError.message });
-      }
-
-      res.json({ id: insertedRow.insertId, ...req.body });
-    } catch (catchError) {
-      console.error('Error creating listing:', catchError);
-      return res
-        .status(500)
-        .json({ error: 'An error occurred while creating listing' });
-    }
-  },
-);
+    res.json({ id: insertedRow.insertId, ...req.body });
+  } catch (catchError) {
+    console.error('Error creating listing:', catchError);
+    return res
+      .status(500)
+      .json({ error: 'An error occurred while creating listing' });
+  }
+});
 
 export default listingsRouterFormAndFiles;
