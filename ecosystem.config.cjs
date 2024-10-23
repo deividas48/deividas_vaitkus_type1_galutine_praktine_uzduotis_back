b95bc4ae20ad5ae03a611a6ff72fd27807a1c105
deividas48 <deividas.v6@gmail.com>
2024-10-23 15:23:13 +0300

    fix:

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,15 @@
-// ecosystem.config.js
-// The ecosystem.config.js file for PM2 helps manage environment variables by
+// ecosystem.config.cjs
+// The ecosystem.config.cjs file for PM2 helps manage environment variables by
 // specifying which environment settings to use when running your application
 //  Instead, it sets environment variables, such as NODE_ENV=production
-// pm2 starts ecosystem.config.js
+// pm2 starts ecosystem.config.cjs
 
 module.exports = {
   apps: [
     {
       name: 'teikas-backend-api', // random name
       script: 'src/server.js',
-      env: {
+      env_production: {
         NODE_ENV: 'production',
         PORT: 3000, // Set other variables as needed
       },
