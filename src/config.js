68201c032a0faa9aad85cf557cc934251a044b09
deividas48 <deividas.v6@gmail.com>
2024-10-29 06:10:49 +0200

    fix: connect to sql in production enviroment

--- src/config.js
+++ src/config.js
@@ -1,13 +1,26 @@
 // src\config.js
 
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });
+} else {
+  dotenv.config(); // Defaults to loading .env in development
+}
+
+// Environment-based .env file
+const whichEnv = process.env.NODE_ENV === 'production'
+  ? dotenv.config({ path: '.env.production' })
+  : dotenv.config();
+
 export const dbConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  user: whichEnv.parsed.DB_USER,
+  password: whichEnv.parsed.DB_PASS,
+  host: whichEnv.parsed.DB_HOST,
+  database: whichEnv.parsed.DB_DATABASE,
+  port: whichEnv.parsed.DB_PORT,
 };
 
 export const { PORT } = process.env;
 
-// console.log('dbConfig ===', dbConfig);
+// console.log('dbConfig ===', dbConfig, process.env.NODE_ENV);
